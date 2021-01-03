58256687d9f1f19b69419c5fa419cbc880ae2558
Harvey Bates <phenobottle@gmail.com>
2021-01-03 20:37:36 +1100

    restructure sine function

--- C++AnimationFramework/main.cpp
+++ C++AnimationFramework/main.cpp
@@ -1,41 +1,56 @@
 #include "raylib.h"
+#include <iostream>
 #include <math.h>
 
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-void draw_grid(Vector2 gridStart,
-               float extentWidth,
-               float extentHeight,
-               float hLines,
-               float vLines,
-               float lineWidth,
-               Color lineColor){
-    
-    float densityH = extentHeight / hLines;
-    float densityV = extentWidth / vLines;
-    
-    Vector2 startHLine = {gridStart.x, gridStart.y};
-    Vector2 endHLine = {extentWidth + gridStart.x, gridStart.y};
-    DrawLineEx(startHLine, endHLine, lineWidth, lineColor);
-    for(int i = 0; i < hLines; i++){
-        Vector2 startHLine = {gridStart.x, gridStart.y + densityH};
-        Vector2 endHLine = {extentWidth + gridStart.x, gridStart.y + densityH};
-        DrawLineEx(startHLine, endHLine, lineWidth, lineColor);
-        densityH += extentHeight / hLines;
-    }
 
-    densityH = extentHeight / hLines;
-    Vector2 startVLine = {gridStart.x, gridStart.y};
-    Vector2 endVLine = {gridStart.x, extentHeight + gridStart.y};
-    DrawLineEx(startVLine, endVLine, lineWidth, lineColor);
-    for(int i = 0; i < vLines; i++){
-        Vector2 startVLine = {gridStart.x + densityV, gridStart.y};
-        Vector2 endVLine = {gridStart.x + densityV, extentHeight + gridStart.y};
-        DrawLineEx(startVLine, endVLine, lineWidth, lineColor);
-        densityV += extentWidth / vLines;
+
+class Grid{
+    float hLines = 10;
+    float vLines = 10;
+public:
+    void set_horizontal_lines(float num){
+        num = hLines;
     }
-}
+    void set_vertical_lines(float num){
+        num = vLines;
+    }
+    void draw(Vector2 gridStart,
+              float extentWidth,
+              float extentHeight,
+              float hLines,
+              float vLines,
+              float lineWidth,
+              Color lineColor){
+        
+        float densityH = extentHeight / hLines;
+        float densityV = extentWidth / vLines;
+
+        Vector2 startHLine = {gridStart.x, gridStart.y};
+        Vector2 endHLine = {extentWidth + gridStart.x, gridStart.y};
+        DrawLineEx(startHLine, endHLine, lineWidth, lineColor);
+        for(int i = 0; i < hLines; i++){
+           Vector2 startHLine = {gridStart.x, gridStart.y + densityH};
+           Vector2 endHLine = {extentWidth + gridStart.x, gridStart.y + densityH};
+           DrawLineEx(startHLine, endHLine, lineWidth, lineColor);
+           densityH += extentHeight / hLines;
+        }
+
+        densityH = extentHeight / hLines;
+        Vector2 startVLine = {gridStart.x, gridStart.y};
+        Vector2 endVLine = {gridStart.x, extentHeight + gridStart.y};
+        DrawLineEx(startVLine, endVLine, lineWidth, lineColor);
+        for(int i = 0; i < vLines; i++){
+           Vector2 startVLine = {gridStart.x + densityV, gridStart.y};
+           Vector2 endVLine = {gridStart.x + densityV, extentHeight + gridStart.y};
+           DrawLineEx(startVLine, endVLine, lineWidth, lineColor);
+           densityV += extentWidth / vLines;
+        }
+    }
+};
+
 
 
 void draw_origin(Vector2 origin,
@@ -68,18 +83,36 @@ void draw_origin(Vector2 origin,
     }
 }
 
-void insert_sine_wave(Vector2 startPoint,
-                      float width,
-                      float height,
-                      float amp,
-                      float xPos,
-                      float freq){
-    for(float x = startPoint.x; x < startPoint.x + width; ++x){
-        float y = amp * sin((x * (PI/180)) * freq + xPos) + height;
-        Vector2 point = {x, y};
-        DrawCircleV(point, 2.0f, RED);
+
+class Sinewave{
+    float freqency, amplitude;
+public:
+    void set_frequecy(float freq){
+        freq = freqency;
     }
-}
+    void set_amplitude(float amp){
+        amp = amplitude;
+    }
+    float get_frequency(){return freqency;}
+    float get_amplitude(){return amplitude;}
+    
+    void draw(Vector2 startPoint,
+        float width,
+        float offset,
+        float amp,
+        float xPos,
+        float freq){
+        
+        amplitude = amp;
+        freqency = freq;
+        
+        for(float x = startPoint.x; x < startPoint.x + width; ++x){
+            float y = amp * sin((x * (PI/180)) * freq + xPos) + offset;
+            Vector2 point = {x, y};
+            DrawCircleV(point, 2.0f, RED);
+        }
+    }
+};
 
 
 int main(void)
@@ -93,10 +126,9 @@ int main(void)
     float gridWidth = 300.0f;
     float gridHeight = 320.0f;
     
-    float numHLines = 10.0f;
-    float numVLines = 10.0f;
+    float hLines = 10.0f;
+    float vLines = 10.0f;
     
-    float amp = 150.0f;
     float xPos = 10.0f;
     float freq = 2.0f;
     
@@ -106,16 +138,22 @@ int main(void)
         
         ClearBackground(BLACK);
         
-        draw_grid(gridStart, gridWidth, gridHeight, numHLines, numVLines, 1.0f, LIGHTGRAY);
+        Grid grid;
+        grid.draw(gridStart, gridWidth, gridHeight, hLines, vLines, 1.0f, LIGHTGRAY);
         
         Vector2 origin = {gridStart.x, gridStart.y + gridHeight};
         
         draw_origin(origin, true, gridWidth / 2.0f, gridHeight  / 2.0f, WHITE);
         
-        insert_sine_wave(gridStart, gridWidth, gridStart.y + (gridHeight / 2), amp, xPos, freq);
+        Vector2 sineStart = {400, 200};
+        Sinewave sinewave;
+        sinewave.draw(sineStart, 200.0f, 100.0f, 50.0f, xPos, freq);
+        xPos += 0.01f;
+        DrawText(std::to_string(sinewave.get_frequency()).c_str(), 650, 100, 25, WHITE);
+        DrawText(std::to_string(sinewave.get_amplitude()).c_str(), 650, 140, 25, WHITE);
         
         EndDrawing();
-        xPos += 0.05f;
+        
     }
 
     CloseWindow();
